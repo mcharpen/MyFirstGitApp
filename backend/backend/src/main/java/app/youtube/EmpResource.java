5d89229717d0908fe508d81757ddfe2dc3617d8c
mcharpen <marc.charpentier@icloud.com>
2025-10-30 17:16:52 +0100

    Insert Data into EMP

--- backend/backend/src/main/java/app/youtube/EmpResource.java
+++ backend/backend/src/main/java/app/youtube/EmpResource.java
@@ -9,22 +9,6 @@ import jakarta.enterprise.context.ApplicationScoped;
 import java.util.List;
 import java.math.BigDecimal;
 
-@Entity
-@Table(name = "emp")
-class Emp {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public Long id;
-    @Column(name = "first_name")
-    public String firstName;
-    @Column(name = "last_name")
-    public String lastName;
-    @Column(name = "birth_date")
-    public java.sql.Date birthDate;
-    @Column(name = "salary")
-    public BigDecimal salary;
-}
-
 @Path("/emp")
 @ApplicationScoped
 @Produces(MediaType.APPLICATION_JSON)
