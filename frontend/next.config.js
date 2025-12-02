e125f4a0593988b93dfadc1ce474911d7749f25a
mcharpen <marc.charpentier@icloud.com>
2025-12-02 17:05:32 +0100

    test

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/libertyX',
+  assetPrefix: '/libertyX',
   // output: 'export', // static export disabled for server mode
 }
 
